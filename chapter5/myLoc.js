1847c4f957022c2aba40693ea3d73e9851214aa3
xlf655 <269162281@qq.com>
2017-09-19 18:28:59 +0800

    更新位置
    更新位置属性

---
+++ chapter5/myLoc.js
@@ -0,0 +1,28 @@
+window.onload=getMyLocation;
+function getMyLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayLocation,displayError);
+  } else {
+    alert("Oops, no geolocation support")
+  }
+}
+function displayLocation(position){
+  var latitude=position.coords.latitude;
+  var longitude=position.coords.longitude;
+  var div=document.getElementById("location");
+  div.innerHTML="You are at latitude: "+latitude+", Longitude: "+longitude;
+}
+function displayError(error) {
+  var errorTypes={
+    0:"Unknown error",
+    1:"Permission denied by user",
+    2:"Position is not available",
+    3:"Request timed out"
+  };
+  var errorMessage=errorTypes[error.code];
+  if (error.code==0||error.code==2) {
+    errorMessage=errorMessage+""+error.message;
+  }
+  var div=document.getElementById("location");
+  div.innerHTML=errorMessage
+}
